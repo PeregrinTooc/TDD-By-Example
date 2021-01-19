affde6045b826cef9250f499617c702957a204d4
Philipp Rembold <Philipp.Rembold@sap.com>
2021-01-19 17:17:56 +0100

    Make Franc work

--- src/multiCurrency.py
+++ src/multiCurrency.py
@@ -2,11 +2,26 @@
 #table lookup?
 #equal null?
 #equal other object?
+# CHF5 *2 = CHF 10
+# Dollar/Franc Duplication
+# Common _eeq__
+# Common times
+
 
 from unittest.result import TestResult
 
 
 class Dollar:
+    def __init__(self,amount):
+        self._amount = amount
+
+    def times(self,multiplicator):
+        return Dollar(self._amount * multiplicator)
+    
+    def __eq__(self, other):
+        return self._amount == other._amount
+
+class Franc:
     def __init__(self,amount):
         self._amount = amount
 
--- src/test_suite.py
+++ src/test_suite.py
@@ -1,8 +1,9 @@
 import unittest
 from multiCurrency import Dollar
+from multiCurrency import Franc
 
 
-class multiplication(unittest.TestCase):
+class testDollar(unittest.TestCase):
     def testMultiplication(self):
         fiveDollars = Dollar(5)
         self.assertEquals(Dollar(10),fiveDollars.times(2))
@@ -11,3 +12,10 @@ class multiplication(unittest.TestCase):
     def testEquality(self):
         self.assertEquals(Dollar(5), Dollar(5))
         self.assertNotEqual(Dollar(5), Dollar(6))         
+
+class testFranc(unittest.TestCase):
+    def testMultiplication(self):
+        fiveDollars = Franc(5)
+        self.assertEquals(Franc(10),fiveDollars.times(2))
+        self.assertEquals(Franc(15),fiveDollars.times(3))
+
