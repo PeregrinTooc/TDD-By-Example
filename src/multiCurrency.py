1ea4a056e1e2588cdbbacd59dbe058dc9830bebe
Philipp Rembold <Philipp.Rembold@sap.com>
2021-01-20 13:02:51 +0100

    Can add two moneys of same currency and prepared for exchange rates

--- src/multiCurrency.py
+++ src/multiCurrency.py
@@ -6,12 +6,31 @@
 
 from abc import ABC, abstractmethod 
 
+class Bank:
+    def reduce(self, source, to):
+        return source.reduce(to)
+        
+class Expression(ABC):
+    @abstractmethod
+    def reduce(self, to):
+        pass
+    
+
 def makeDollar(amount):
     return Money(amount, 'USD')
 
 def makeFranc(amount):
     return Money(amount, 'CHF')
-class Money:
+
+class Sum(Expression):
+    def __init__(self,augend,addend):
+        self._augend = augend
+        self._addend = addend
+    
+    def reduce(self,to):
+        amount = self._augend.getAmount() + self._addend.getAmount()
+        return Money(amount, to)
+class Money(Expression):
     def __init__(self,amount,currency):
         self._amount = amount
         self._currency = currency
@@ -25,3 +44,11 @@ class Money:
     def getCurrency(self):
         return self._currency
     
+    def getAmount(self):
+        return self._amount
+    
+    def __add__(self, other):
+        return Sum(self, other)
+    
+    def reduce(self,to):
+        return self
--- src/test_suite.py
+++ src/test_suite.py
@@ -1,8 +1,7 @@
-#Delete testFranc for Multiplication?  
 
 import unittest
 import multiCurrency
-from multiCurrency import Money
+from multiCurrency import Money, makeDollar
 
 
 class testMultiplication(unittest.TestCase):
@@ -29,3 +28,30 @@ class TestCurrency(unittest.TestCase):
     
     def testDollar(self):
         self.assertEquals(multiCurrency.makeDollar(1).getCurrency(), 'USD')
+
+class testAddition(unittest.TestCase):
+    def setUp(self):
+        super().setUp()    
+        self.bank = multiCurrency.Bank()
+    def testSameCurrency(self):
+        sum = multiCurrency.makeDollar(5) + multiCurrency.makeDollar(5)
+        reduced = self.bank.reduce(sum, 'USD')
+        self.assertEquals(reduced, multiCurrency.makeDollar(10))
+    
+    def testPlusReturnsASum(self):
+        five = makeDollar(5)
+        sum = five + five
+        self.assertEquals(sum._augend, five)
+        self.assertEquals(sum._addend, five)
+
+
+class testReduce(unittest.TestCase):
+    def setUp(self):
+        self.cut = multiCurrency.Bank().reduce
+    
+    def testReduceSum(self):
+        sum = multiCurrency.makeDollar(3) + multiCurrency.makeDollar(4)
+        self.assertEqual(self.cut(sum, 'USD'),multiCurrency.makeDollar(7))
+    
+    def testReduceMoney(self):
+        self.assertEqual(self.cut(makeDollar(7), 'USD'),multiCurrency.makeDollar(7))
