fa294a1d06424ace8a1eda7e167bcd6be4b113f8
Philipp Rembold <Philipp.Rembold@sap.com>
2021-01-20 11:51:10 +0100

    Introduce Create Functions

--- src/multiCurrency.py
+++ src/multiCurrency.py
@@ -3,17 +3,26 @@
 #equal null?
 #equal other object?
 # Common times
+#Currency?
 
 
+from abc import ABC, abstractmethod 
 
-from unittest.result import TestResult
+def makeDollar(amount):
+    return Dollar(amount)
 
-class Money:
+def makeFranc(amount):
+    return Franc(amount)
+class Money(ABC):
     def __init__(self,amount):
         self._amount = amount
 
     def __eq__(self, other):
-        return self._amount == other._amount
+        return ( self._amount == other._amount and self.__class__ == other.__class__ )
+    
+    @abstractmethod
+    def times(self, multiplicator):
+        pass
 class Dollar(Money):
 
     def times(self,multiplicator):
--- src/test_suite.py
+++ src/test_suite.py
@@ -1,25 +1,30 @@
+#Delete testFranc for Multiplication?
+
 import unittest
-from multiCurrency import Dollar
-from multiCurrency import Franc
+import multiCurrency
+from multiCurrency import Money
 
 
-class testDollar(unittest.TestCase):
-    def testMultiplication(self):
-        fiveDollars = Dollar(5)
-        self.assertEquals(Dollar(10),fiveDollars.times(2))
-        self.assertEquals(Dollar(15),fiveDollars.times(3))
+class testMultiplication(unittest.TestCase):
+    def testDollar(self):
+        fiveDollars = multiCurrency.makeDollar(5)
+        self.assertEquals(multiCurrency.makeDollar(10),fiveDollars.times(2))
+        self.assertEquals(multiCurrency.makeDollar(15),fiveDollars.times(3))
 
-    def testEquality(self):
-        self.assertEquals(Dollar(5), Dollar(5))
-        self.assertNotEqual(Dollar(5), Dollar(6))         
-
-class testFranc(unittest.TestCase):
-    def testMultiplication(self):
-        fiveDollars = Franc(5)
-        self.assertEquals(Franc(10),fiveDollars.times(2))
-        self.assertEquals(Franc(15),fiveDollars.times(3))
+    def testFranc(self):
+        fiveFrancs = multiCurrency.makeFranc(5)
+        self.assertEquals(multiCurrency.makeFranc(10),fiveFrancs.times(2))
+        self.assertEquals(multiCurrency.makeFranc(15),fiveFrancs.times(3))
     
-    def testEquality(self):
-        self.assertEquals(Franc(5), Franc(5))
-        self.assertNotEqual(Franc(5), Franc(6))         
 
+class testEquality(unittest.TestCase):
+    def testDollar(self):
+        self.assertEquals(multiCurrency.makeDollar(5), multiCurrency.makeDollar(5))
+        self.assertNotEqual(multiCurrency.makeDollar(5), multiCurrency.makeDollar(6))         
+    
+    def testFranc(self):
+        self.assertEquals(multiCurrency.makeFranc(5), multiCurrency.makeFranc(5))
+        self.assertNotEqual(multiCurrency.makeFranc(5), multiCurrency.makeFranc(6))         
+
+    def testMixed(self):
+        self.assertNotEqual(multiCurrency.makeFranc(5),multiCurrency.makeDollar(5))
