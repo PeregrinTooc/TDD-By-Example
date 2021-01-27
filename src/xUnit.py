89e4d00eb4269cfe61e330b2eb8b64e194132447
Philipp Rembold <Philipp.Rembold@sap.com>
2021-01-27 14:28:45 +0100

    tearDown

--- src/xUnit.py
+++ src/xUnit.py
@@ -1,4 +1,3 @@
-# Invoke tearDown afterwards
 # Invoke tearDown afterwards even if testmethod fails
 # Run multiple tests
 # Report collected results
@@ -10,8 +9,12 @@ class TestCase():
         self.setUp()
         method = getattr(self, self.name)
         method()
+        self.tearDown()
 
     def setUp(self):
+        pass
+
+    def tearDown(self):
         pass
 
 
@@ -25,12 +28,15 @@ class WasRun(TestCase):
     def setUp(self):
         self.log = "setup "
 
+    def tearDown(self):
+        self.log += "tearDown "
+
 
 class TestCaseTest(TestCase):
     def testTemplateMethod(self):
         self.cut = WasRun("testMethod")
         self.cut.run()
-        assert(self.cut.log.count("setup testMethod ") == 1)
+        assert(self.cut.log.count("setup testMethod tearDown ") == 1)
 
 
 TestCaseTest("testTemplateMethod").run()
